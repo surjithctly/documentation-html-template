6a35cdd866213078b774dc42e2c8937741068991
surjithctly <surjithctly@gmail.com>
2015-07-14 12:54:25 +0530

    initial commit
    initial commit

---
+++ js/custom.js
@@ -0,0 +1,69 @@
+(function($) {
+    "use strict";
+
+    /* ==============================================
+    ANIMATION -->
+    =============================================== */
+
+    new WOW({
+        boxClass: 'wow', // default
+        animateClass: 'animated', // default
+        offset: 0, // default
+        mobile: true, // default
+        live: true // default
+    }).init();
+
+    /* ==============================================
+    LIGHTBOX -->
+    =============================================== */
+
+    jQuery('a[data-gal]').each(function() {
+        jQuery(this).attr('rel', jQuery(this).data('gal'));
+    });
+    jQuery("a[data-rel^='prettyPhoto']").prettyPhoto({
+        animationSpeed: 'slow',
+        theme: 'light_square',
+        slideshow: true,
+        overlay_gallery: true,
+        social_tools: false,
+        deeplinking: false
+    });
+
+    /* ==============================================
+    SCROLL -->
+    =============================================== */
+
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+
+    /* ==============================================
+    VIDEO FIX -->
+    =============================================== */
+
+    $(document).ready(function() {
+        // Target your .container, .wrapper, .post, etc.
+        $(".media").fitVids();
+    });
+
+    /* ==============================================
+    VIDEO FIX -->
+    =============================================== */
+
+    $('.intro>li>a').click(function() {
+        $('.intro>li').removeClass('active');
+        $(this).parent().addClass('active');
+    });
+
+})(jQuery);
